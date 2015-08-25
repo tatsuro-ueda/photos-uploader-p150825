e0c7031281f181c367c226327aee76dc94215e22
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2015-08-25 16:43:22 +0900

    ➕初期化した

---
+++ photos-uploader-p150825/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photos-uploader-p150825
+//
+//  Created by Tatsuro Ueda on 2015/08/25.
+//  Copyright (c) 2015年 Tatsuro Ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
